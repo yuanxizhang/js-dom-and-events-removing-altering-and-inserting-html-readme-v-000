20c878d1e73340c86b3c394a6b4f163c84c81fb6
yuanxizhang <yuanxi.zhang@gmail.com>
2019-06-30 23:17:31 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,15 @@
-let element = document.createElement('div');
-
+var element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-element.style.textAlign = 'center';
 document.body.appendChild(element);
-
-let ul = document.createElement('ul');
+element.style.textAlign = 'center';
+var ul = document.createElement('ul')
  
 for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li');
-  li.innerHTML = (i + 1).toString();
-  ul.appendChild(li);
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
 }
  
-element.appendChild(ul);
-ul.style.textAlign = 'left';
+element.appendChild(ul)
 
-ul.removeChild(ul.querySelector('li:nth-child(2)'));
-ul.remove();
